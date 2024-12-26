5e26a5f4563602b4392e39aff7356c91e89ec8c4
Groot <groot@redraccoon.kr>
2024-12-26 14:57:42 +0900

    TEST

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 Groot Cyber War Room",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
