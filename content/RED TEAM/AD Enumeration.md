d1a6a0ca4c893e70965e0f2c17261695feead75b
Groot <groot@redraccoon.kr>
2024-12-26 15:11:31 +0900

    test

--- content/RED TEAM/AD Enumeration.md
+++ content/RED TEAM/AD Enumeration.md
@@ -6,6 +6,22 @@ tags:
 
 comments: true
 enableToc: true
+
+<script src="https://giscus.app/client.js"
+        data-repo="grootredraccoon/groot-publish"
+        data-repo-id="R_kgDONiU_jw"
+        data-category="Announcements"
+        data-category-id="DIC_kwDONiU_j84Clhb9"
+        data-mapping="pathname"
+        data-strict="0"
+        data-reactions-enabled="1"
+        data-emit-metadata="0"
+        data-input-position="bottom"
+        data-theme="preferred_color_scheme"
+        data-lang="en"
+        crossorigin="anonymous"
+        async>
+</script>
 ---
 
 안녕하세요
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'grootredraccoon/groot-publish',
+        // from data-repo-id
+        repoId: 'R_kgDONiU_jw',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDOFxRnmM4B-Xg6',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
